690134053dd3a3e2623d18ec09f0852b30a3a758
Alexander Kosik <akosik@t-online.de>
2022-02-19 16:18:23 +0100

    Add . and character classes to dfa

--- content/engine/dfa.md
+++ content/engine/dfa.md
@@ -1,5 +1,24 @@
 [Back](./engine.md) | [Next](./dfa.md)
 
 # Building a DFA machine
+To build a DFA RegEx engine we need to implement a finite state machine. 
+
+A finite state machine consists of **states** and **events**. With events we can transmit from one state to another (or also stay in the same state). But we can only be at one state at any given time.
+
+Let's have a look a the example from the wikipedia page of finite state machines. 
+
+# Turnstile as a state machine
+If we look at as turnstile as a state machine it has two states: it can be *locked* and no one can pass it, or it can be *unlocked* and let exactly one person through. 
+
+![Turnstile](images/turnstile.jpg "Turnstile")
+
+To start a transition from state *locked* to *unlocked* we need an event to occure: this event is inserting a coin. Once we inserted a coin we can get through the turnstile by pushing and therfore implicitly changing its state from *unlocked* to *locked*. 
+
+Drawn as a state machine the descibed behaviour looks like this:
+![turnstile state machine](images/state_machine_turnstile.png "Turnstile State Machine")
+
+## Implementing a state machine in `python`
+
+We now look at multiple implementations of a state machine in python. 
 
 ## A regular expression as a Finite State Machine
--- content/engine/table_fsm.py
+++ content/engine/table_fsm.py
@@ -1,15 +1,6 @@
 #!/usr/bin/env python3
 
 
-class States:
-    LOCKED = 0
-    UNLOCKED = 1
-
-
-class Events:
-    PUSH = 0
-    COIN = 1
-
 def ascii_to_number(char):
     return ord(char) - 97
 
@@ -18,25 +9,44 @@ class FSM:
         self.states = []
         self.cs = 0
 
-    def add_state(self, transition_char):
+    def add_state(self, *tcs):
         state = [0] * 26
-        state[ascii_to_number(transition_char)] = len(self.states) + 1
+        if ... in tcs:
+            for a in range(ascii_to_number(tcs[0]), ascii_to_number(tcs[-1])):
+                state[a] = len(self.states) + 1
+        else:
+            for a in tcs:
+                state[ascii_to_number(a)] = len(self.states) +1
         self.states.append(state)
 
     def next_state(self, char):
         self.cs = self.states[self.cs][ascii_to_number(char)]
 
 
-    def __str__(self):
-        s = ""
-        for row in zip(*self.states):
-            s += " ".join(str(c) for c in row) + "\n"
-        return s
-
     def compile(self, regex):
         self.states = []
-        for char in regex:
-            self.add_state(char)
+        iter_ = iter(regex)
+        for char in iter_:
+            match char:
+                case '.':
+                    self.add_state('a', ..., 'z')
+                case '[':
+                    try:
+                        # collect every character within []
+                        valid = []
+
+                        while char := next(iter_):
+                            if char == ']':
+                                break
+                            valid.append(char)
+
+                        self.add_state(*valid)
+                    except StopIteration:
+                        print("Invalid regex: ] expected")
+                        return 
+
+                case _:
+                    self.add_state(char)
 
     def match(self, input_str):
         self.cs = 0
@@ -46,3 +56,10 @@ class FSM:
                 return False
         return self.cs == len(self.states)
 
+    def __str__(self):
+        s = ""
+        for row in zip(*self.states):
+            s += " ".join(str(c) for c in row) + "\n"
+        return s
+    
+    __repr__ = __str__
